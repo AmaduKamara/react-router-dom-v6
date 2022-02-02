88fab2d050830c292586ae317f6095c2ef7ff81e
Amkam <amaduskamara@gmail.com>
2022-02-02 15:03:58 +0000

    Add Tab buttons in Learn page for routing

--- src/index.js
+++ src/index.js
@@ -3,14 +3,19 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import Home from "./pages/Home";
 
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import Learn from "./pages/Learn";
 
 ReactDOM.render(
   <Router>
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="myapps" element={<Learn />} />
+      <Route path="myapps" element={<Navigate replace to="/learn" />} />
       <Route path="learn" element={<Learn />} />
     </Routes>
   </Router>,
--- src/pages/Learn.js
+++ src/pages/Learn.js
@@ -1,11 +1,19 @@
-import React from "react";
+import { Link } from "react-router-dom";
 
 const Learn = () => (
-  <div className="container mx-auto my-16">
+  <div className="container mx-auto my-16 text-center">
     <h1 className="text-3xl text-center font-bold">Courses Page</h1>
     <p className="text-center mt-3 text-lg">
       Find all courses here and select which you want to take
     </p>
+    <div className="mt-10">
+      <Link className="mx-10 border py-3 px-6 shadow-lg" to="learn/courses">
+        Courses
+      </Link>
+      <Link className="mx-10 border py-3 px-6 shadow-lg" to="learn/bundles">
+        Courses
+      </Link>
+    </div>
   </div>
 );
 
