1fd9f027f6965aeb6f43fd61511505e022a96c10
Amkam <amaduskamara@gmail.com>
2022-02-02 17:34:36 +0000

    Import and use useNavigate hook from v6

--- src/components/CourseDetails.jsx
+++ src/components/CourseDetails.jsx
@@ -1,11 +1,16 @@
-import { useParams,  } from "react-router-dom";
+import { useParams, useNavigate, useLocation } from "react-router-dom";
 
 const CourseDetails = () => {
+  const navigate = useNavigate();
+
   const { courseid } = useParams();
   return (
     <div>
       <h3>URL of a selected course: {courseid}</h3>
-      <button className="py-3 px-6 border bg-teal-400 mt-5">
+      <button
+        onClick={() => navigate("/course-price-details")}
+        className="py-3 px-6 border bg-teal-400 mt-5"
+      >
         View Course Price
       </button>
     </div>
