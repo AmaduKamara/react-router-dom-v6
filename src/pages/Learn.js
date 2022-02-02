9fb0be449c2223c870e86747a158fab937c48d13
Amkam <amaduskamara@gmail.com>
2022-02-02 14:51:56 +0000

    Add Learn component for all courses

---
+++ src/pages/Learn.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Learn = () => (
+  <div className="container mx-auto my-16">
+    <h1 className="text-3xl text-center font-bold">Courses Page</h1>
+    <p className="text-center mt-3 text-lg">
+      Find all courses here and select which you want to take
+    </p>
+  </div>
+);
+
+export default Learn;
