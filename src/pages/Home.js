26a42650d177fed64f83f07767737ed7b2c4733f
Amkam <amaduskamara@gmail.com>
2022-02-02 15:20:05 +0000

    Add button link in Home page to go to Learn page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => (
-  <div className="container mx-auto my-16">
-    <h1 className="text-3xl text-center font-bold">Home Component</h1>
+  <div className="container mx-auto my-16 text-center">
+    <h1 className="text-3xl text-center font-bold">Home Component</h1>.
+    <div className="mt-16">
+      <Link
+        to="/learn"
+        className="py-4 px-6 rounded-full border bg-cyan-500 text-white uppercase"
+      >
+        Get Started
+      </Link>
+    </div>
   </div>
 );
 
